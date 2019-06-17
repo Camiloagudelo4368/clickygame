a0d850768ef393e1388e6fe21f9269f4fafc6238
Camilo Agudelo <camilo@Camilos-MacBook-Pro-2.local>
2019-06-17 00:14:48 -0400

    Updates

---
+++ precache-manifest.2b0476fb3204266917b5d68631e83322.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb73e0d20d81a49d2ae195c3319ca71",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "eafef8a7c2d87c75e548",
+    "url": "/clickygame/static/css/main.a73bc4cf.chunk.css"
+  },
+  {
+    "revision": "38e276c670e3ce5c1ff6",
+    "url": "/clickygame/static/js/2.785f2b4b.chunk.js"
+  },
+  {
+    "revision": "eafef8a7c2d87c75e548",
+    "url": "/clickygame/static/js/main.cbab424f.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.de87b5f01d47eb19ee513b8b623f2d00.js"
+  "/clickygame/precache-manifest.2b0476fb3204266917b5d68631e83322.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clickygame/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
