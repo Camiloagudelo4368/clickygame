71dc6bff3b3fa9cda098699f4bd35b2a4a12cbdc
Camilo Agudelo <camilo@Camilos-MBP-2.lan1>
2019-10-22 00:39:05 -0400

    Updates

---
+++ precache-manifest.19c797d6f674fc02020e9eaab0f76921.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab380883842bb730b9586af03d94b89",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "0d652891736f32fcf1cc",
+    "url": "/clickygame/static/css/main.50028a37.chunk.css"
+  },
+  {
+    "revision": "2796696533196ff8b652",
+    "url": "/clickygame/static/js/2.0859c95b.chunk.js"
+  },
+  {
+    "revision": "0d652891736f32fcf1cc",
+    "url": "/clickygame/static/js/main.2be6c9ed.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
